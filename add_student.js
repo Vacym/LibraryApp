550ecc4c72e05c6969c880e19c36c9e4245ee9ba
Koe-kto <65008219+Pavlik-g@users.noreply.github.com>
2020-10-06 17:07:13 +0300

    Add files via upload

---
+++ add_student.js
@@ -0,0 +1,12 @@
+//version 0.0.0.1
+function get_new_student() {
+    //Получение данных с полей ввода
+    console.log("запущено")
+    var name = document.getElementById("name").value
+    var surname = document.getElementById("surname").value
+    var lastname = document.getElementById("lastname").value
+    var class_number = document.getElementById("class_number").value
+    var class_latter = document.getElementById("class_latter").value
+    console.log(name, surname, lastname, class_number, class_latter);
+    //Можно использовать далее как хочется
+}
