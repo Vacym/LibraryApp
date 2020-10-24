d39e2920b0b0e0cd3964f3e3cf2f7fa1ff9a50f9
Djacon <62030101+Djacon@users.noreply.github.com>
2020-10-24 18:19:09 +0300

    Добавил вывод таблицы с твоими блоками

--- mysite/search_student.php
+++ mysite/search_student.php
@@ -6,9 +6,8 @@
     <meta charset="UTF-8">
     <title>Найти ученика</title>
 
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" href="sources/style/search_student.css">
-    <script src="sources/js/find_user.js"></script>
+    <link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="sources/style/search_student.css">
 </head>
 
 <body>
@@ -39,7 +38,26 @@
 
         while ($st = mysqli_fetch_assoc($result))
         {
-            echo '<div class="search_result">', $st['Name'], ' ', $st['Surname'], ' ', $st['Lastname'], '</div>';
+        	echo '<div class="search_result">';
+        	echo '<div class="result">';
+        	echo '<div class="personal_information">';
+        	echo '<div class="FCS">';
+        	echo '<div class="class">', $st['Class'], $st['Letter'], '</div>';
+        	echo $st['Surname'], ' ', $st['Name'], ' ', $st['Lastname'];
+        	echo '</div>';
+        	echo '</div>';
+        	echo '<div class="books">';
+
+        	$res = mysqli_query($mysql, "SELECT * FROM `books`");
+
+        	while ($bk = mysqli_fetch_assoc($res)) {
+        		echo '<div class="one_book">';
+        		echo '<span class="name_book">', $bk['Name'], '</span>';
+        		echo '<span class="autor_book">', $bk['Author'] , '</span>';
+        		echo '</div>';
+        	}
+        	
+        	echo '</div></div></div>';
         }
         
     ?>
