74fb6fea0d64b6586f6c1f157f4432673331a8d0
Djacon <62030101+Djacon@users.noreply.github.com>
2020-10-24 21:31:29 +0300

    Исправил все что нужно

--- mysite/sources/php/check_bk.php
+++ mysite/sources/php/check_bk.php
@@ -1,28 +1,31 @@
 <?php
-
     if (!count($_POST)){
         echo 'А что мы тут делаем?';
-        exit();
     }
+    else {
+        $name    = $_POST['name'];
+        $author  = $_POST['author'];
+        $genre   = $_POST['genre'];
+        $comment = $_POST['comment'];
 
-    $name    = $_POST['name'];
-    $author  = $_POST['author'];
-    $genre   = $_POST['genre'];
-    $comment = $_POST['comment'];
+        $valid_name    = preg_match("/^[а-я- ]+$/ui", $name);
+        $valid_author  = preg_match("/^[а-я- ]+$/ui", $author);
+        $valid_genre   = preg_match("/^[а-я- ]+$/ui", $genre);
+        $valid_comment = preg_match("/^[а-я- ]+$/ui", $comment);
 
-    $valid_name    = preg_match("/^[а-я-]+$/ui", $name);
-    $valid_author  = preg_match("/^[а-я-]+$/ui", $author);
-    $valid_genre   = preg_match("/^[а-я-]+$/ui", $genre);
-    $valid_comment = preg_match("/^[а-я-]+$/ui", $comment);
+        if (!$valid_name || !$valid_author || !$valid_genre) {
+            echo 'Неправильно введенные данные!';
+        }
+        else {
 
-    if (!$valid_name || !$valid_author || !$valid_genre || !$valid_comment) {
-        echo 'Неправильно введенные данные!';
-        exit();
+            $mysql = new mysqli('localhost', 'root', '', 'test');
+            $mysql->query("INSERT INTO `books` (`Name`, `Author`, `Genre`, `Comment`) VALUES ('$name', '$author', '$genre', '$comment')");
+            $mysql->close();
+
+            echo 'Успешно отправлено';
+        }
     }
-
-    $mysql = new mysqli('localhost', 'root', '', 'test');
-    $mysql->query("INSERT INTO `books` (`Name`, `Author`, `Genre`, `Comment`) VALUES ('$name', '$author', '$genre', '$comment')");
-    $mysql->close();
-
-    echo 'Успешно отправлено (все норм)';
+    
+    header("refresh:3;url=../../add_book.html");
+    exit();
 ?>
--- mysite/sources/php/check_st.php
+++ mysite/sources/php/check_st.php
@@ -1,33 +1,37 @@
 <?php
     if (!count($_POST)){
         echo 'А что мы тут делаем?';
-        exit();
     }
+    else {
+	    $firstname = $_POST['firstname'];
+	    $surname   = $_POST['surname'];
+	    $lastname  = $_POST['lastname'];
+	    $class     = $_POST['class'];
+	    $letter    = $_POST['letter'];
 
-    $firstname = $_POST['firstname'];
-    $surname   = $_POST['surname'];
-    $lastname  = $_POST['lastname'];
-    $class     = $_POST['class'];
-    $letter    = $_POST['letter'];
+	    $valid_name = preg_match("/^[а-я-]+$/ui", $firstname);
+	    $valid_surn = preg_match("/^[а-я-]+$/ui", $surname);
+	    $valid_last = preg_match("/^[а-я-]+$/ui", $lastname);
+	    $valid_clss = preg_match("/^([1-9]|1[01])$/", $class);
+	    $valid_lttr = preg_match("/^[А-Я]$/u", $letter);
 
-    if (trim($lastname) == "")
-        $lastname = "без-отчества";
+	    if (trim($lastname) == "") {
+	        $lastname = "Без отчества";
+	        $valid_last = True;
+	    }
 
-    $valid_name = preg_match("/^[а-я-]+$/ui", $firstname);
-    $valid_surn = preg_match("/^[а-я-]+$/ui", $surname);
-    $valid_last = preg_match("/^[а-я-]+$/ui", $lastname);
-    $valid_clss = preg_match("/^([1-9]|1[01])$/", $class);
-    $valid_lttr = preg_match("/^[А-Я]$/u", $letter);
+	    if (!$valid_name || !$valid_surn || !$valid_last || !$valid_clss || !$valid_lttr) {
+	        echo 'Неправильно введенные данные!';
+	    }
+	    else {
+		    $mysql = new mysqli('localhost', 'root', '', 'test');
+		    $mysql->query("INSERT INTO `users` (`Name`, `Surname`, `Lastname`, `Class`, `Letter`) VALUES ('$firstname', '$surname', '$lastname', '$class', '$letter')");
+		    $mysql->close();
 
-    if (!$valid_name || !$valid_surn || !$valid_last || !$valid_clss || !$valid_lttr) {
-        echo 'Неправильно введенные данные!';
-        exit();
-    }
+		    echo 'Успешно отправлено';
+		}
+	}
 
-    // Нам пока что это не нужно
-    $mysql = new mysqli('localhost', 'root', '', 'test');
-    $mysql->query("INSERT INTO `users` (`Name`, `Surname`, `Lastname`, `Class`, `Letter`) VALUES ('$firstname', '$surname', '$lastname', '$class', '$letter')");
-    $mysql->close();
-
-    echo 'Успешно отправлено (все норм)';
+    header("refresh:3;url=../../add_student.html");
+    exit();
 ?>
