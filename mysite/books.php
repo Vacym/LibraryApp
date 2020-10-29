00e3e181fce20540d4d30954a45a3dbd1d321fff
Djacon <62030101+Djacon@users.noreply.github.com>
2020-10-29 17:08:36 +0300

    Update books.php

--- mysite/books.php
+++ mysite/books.php
@@ -2,12 +2,12 @@
 <html>
 <head>
 	<title>Книга</title>
+	
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/sources/style/account.css">
 </head>
 <body>
 	<a class="but" id="home" href="/"></a>
-
-	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="/sources/style/account.css">
 </body>
 
 	
