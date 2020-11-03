98638a639417e78d42f5fb0e5df0c1ead20ef545
Koe-kto <65008219+Pavlik-g@users.noreply.github.com>
2020-11-03 19:24:12 +0300

    Сообщение в отдельных файлах
    Отделил message.(js/css),
    Немного изменил структуру,
    Удалил из js и css отделённые блоки,
    Импортировал js и css в нужные файлы.

--- mysite/sources/js/add.js
+++ mysite/sources/js/add.js
@@ -1,4 +1,4 @@
-//version 2.3
+//version 2.4
 
 function validation_text(element) {
     if (element.currentTarget) {
@@ -101,39 +101,6 @@ function ready() {
     for (let x = 0; x < textarea_list.length; x++) {
         textarea_list[x].addEventListener('input', function(e) { textarea_size(e.target) })
         textarea_size(textarea_list[x])
-    }
-
-    // Всплывающее окно
-
-    class But_message {
-        constructor(button) {
-            this.button = button
-            this.window = document.querySelector(`#${button.getAttribute("message")}`)
-            this.cansel = document.querySelector(`#${button.getAttribute("message")} #cansel`)
-            this.start_hear()
-        }
-        start_hear() {
-            this.button.addEventListener("click", () => this.add_show())
-            this.cansel.addEventListener("click", () => this.del_show())
-        }
-        toggle(a, b) {
-            this.window.classList.add(a)
-            this.window.classList.remove(b)
-        }
-        add_show() {
-            this.window.classList.add("fading")
-            setTimeout(() => this.toggle("show", "fading"), 1)
-        }
-        del_show() {
-            this.toggle("fading", "show")
-            setTimeout(() => this.window.classList.remove("fading"), 250);
-        }
-    }
-
-    let button_list = document.querySelectorAll("[message]")
-    let button_class_list = []
-    for (let x = 0; x < button_list.length; x++) {
-        button_class_list.push(new But_message(button_list[x]))
     }
 }
 
---
+++ mysite/sources/js/message.js
@@ -0,0 +1,37 @@
+function ready() {
+    class But_message {
+        constructor(button) {
+            this.button = button
+            this.window = document.querySelector(`#${button.getAttribute("message")}`)
+            this.cansel = document.querySelector(`#${button.getAttribute("message")} #cansel`)
+            this.start_hear()
+        }
+        start_hear() {
+            this.button.addEventListener("click", () => this.add_show())
+            this.cansel.addEventListener("click", () => this.del_show())
+        }
+        toggle(a, b) {
+            this.window.classList.add(a)
+            this.window.classList.remove(b)
+        }
+        add_show() {
+            this.window.classList.add("fading")
+            setTimeout(() => this.toggle("show", "fading"), 1)
+        }
+        del_show() {
+            this.toggle("fading", "show")
+            setTimeout(() => this.window.classList.remove("fading"), 250);
+        }
+    }
+
+    let button_list = document.querySelectorAll("[message]")
+    let button_class_list = []
+    for (let x = 0; x < button_list.length; x++) {
+        button_class_list.push(new But_message(button_list[x]))
+    }
+}
+
+
+
+
+document.addEventListener("DOMContentLoaded", ready)
