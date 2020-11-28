7ac2c05d2211b115c7327ea29282446a8d24929f
Djacon <62030101+Djacon@users.noreply.github.com>
2020-11-28 22:59:28 +0300

    Добавил твои checkbox'ы и toolbox в свой код

---
+++ books.php
@@ -0,0 +1,165 @@
+<!-- version 1.0 release -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta charset="UTF-8">
+	<title>Книга</title>
+	
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/sources/style/account.css">
+    <link rel="stylesheet" type="text/css" href="/sources/style/message.css">
+    <link rel="stylesheet" type="text/css" href="/sources/style/button.css">
+    <script type="text/javascript" src="/sources/js/add.js"></script>
+    <script type="text/javascript" src="/sources/js/message.js"></script>
+</head>
+
+<body>
+	<a class="but" id="home" href="/"></a>
+
+	<?php
+
+		$url_path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+        $url_parts = explode('/', trim($url_path, ' /'));
+        $page   = array_shift($url_parts);
+        $id     = array_shift($url_parts);
+        $choose = array_shift($url_parts);
+
+        if (!ctype_digit($id)) exit('<h1>Такой страницы не существует!</h1>');
+
+        $mysql = mysqli_connect('localhost', 'root', '', 'Lib');
+        $q = mysqli_query($mysql, "SELECT * FROM `books` WHERE id = '$id'");
+        $book = mysqli_fetch_assoc($q);
+
+        if (is_null($book)) exit('<h1>Такой книги не существует!</h1>');
+
+    	$user_id = $book['User_id'];
+    	$q = mysqli_query($mysql, "SELECT * FROM `users` WHERE id = '$user_id'");
+    	$user = mysqli_fetch_assoc($q);
+
+    	if ($choose == 'edit') {
+
+    		if (count($_POST)){
+                
+                function valid($i, $value) {
+                    $re = array('name' => '/^([а-яё-]|[\., ]|\d)+$/ui', 'num' => "/^\d+$/");
+                    return preg_match($re[$i], $value);
+                }
+
+		        $name    = $_POST['name'];
+                $author  = $_POST['author'];
+                $genre   = $_POST['genre'];
+                $comment = $_POST['comment'];
+                $inv_no  = $_POST['id'];
+
+                $valid_name    = valid('name', $name);
+                $valid_inv_no  = valid('num',  $inv_no);
+                $valid_author  = valid('name', $author);
+                $valid_genre   = valid('name', $genre) || $genre == '';
+                $valid_comment = valid('name', $comment) || $comment == '';
+
+		        if (!$valid_name || !$valid_author || !$valid_genre || !$valid_comment || !$valid_inv_no || mysqli_fetch_assoc(mysqli_query($mysql, "SELECT COUNT(`ID`) as `id` FROM `books` WHERE `Inventory_NO` = '$inv_no' AND `ID` != '$id'"))['id']) {
+		            echo '<h1>Неправильно введенные данные!</h1>';
+
+		            header("refresh:2;url=/books.php/$id/edit");
+		            exit();
+		        }
+
+	            mysqli_query($mysql, "UPDATE `books` SET `Name` = '$name', `Author` = '$author', `Genre` = '$genre', `Comment` = '$comment', `Inventory_NO` = '$inv_no' WHERE `ID` = '$id'");
+
+	            echo '<h1>Книга успешно изменена</h1>';
+
+	            header("refresh:2;url=/books.php/$id");
+                exit();
+            }
+
+    		echo '<div class="box">';
+    		echo '<h1>Редактировать книгу</h1>';
+    		echo '<nav><div href="#" class="but" id="del" message="w_del"></div></nav>';
+    		echo '<form action="" method="POST">';
+
+    		$a = array('id'=> 'ID', 'name' => 'Название', 'author' => 'Автор');
+
+    		foreach ($a as $i => $j) {
+    			echo '<div class="line">';
+                if ($i == 'id') echo "<input type='text' name='$i' id='$i' value='{$book['Inventory_NO']}' class='necessary_input' autocomplete='off'>";
+    			else            echo "<input type='text' name='$i' id='$i' value='{$book[ucfirst($i)]}' class='necessary_input' autocomplete='off'>";
+    			echo "<label for='$i'>$j</label></div>";
+    		}
+
+    		echo '<div class="line">';
+            echo "<input type='text' name='genre' id='genre' value='{$book['Genre']}' autocomplete='off'>";
+            echo '<label for="genre">Жанр</label></div>';
+
+    		echo '<div class="line">';
+    		echo "<textarea type='text' name='comment' id='comment' autocomplete='off'>{$book['Comment']}</textarea>";
+		    echo '<label for="comment">Комментарий</label>';
+		    
+            echo '</div><div><input type="submit" id="submit" value="Сохранить" disabled></div></div>';
+
+            echo '<div class="dark" id="w_del">';
+            echo '<div class="alert_window">';
+            echo '<div class="alert_message">Вы уверены, что хотите удалить книгу?</div>';
+            echo '<div class="sure">';
+            echo '<div class="but_window_space">';
+            echo "<a class='but_window' href='/books.php/$id/delete'>Удалить</a></div>";
+            echo '<div class="but_window_space">';
+            echo '<div class="but_window" id="cancel">Отменить</div>';
+            exit('</div></div></div></div>');
+		    
+    	} else if ($choose == 'delete') {
+                    
+            mysqli_query($mysql, "DELETE FROM `books` WHERE `ID` = '$id'");
+
+            echo '<h1>Книга успешно удалена!</h1>';
+
+            header("refresh:2;url=/");
+            exit();
+    	}
+
+    	echo '<div class="box">';
+    	echo '<div class="head">';
+    	echo "<nav><a href='/books.php/$id/edit' class='but' id='edit'></a></nav>";
+    	echo '<table class="head_information">';
+    	
+    	$arr = array('Inventory_NO' => 'ID', 'Name' => 'Название', 'Author' => 'Автор', 'Genre' => 'Жанр', 'Comment' => 'Описание');
+
+    	foreach ($arr as $i => $j) {
+    		echo '<tr>';
+    		echo "<td class='td_head'>$j: </td>";
+    		echo "<td class='td_value'>$book[$i]</td></tr>";
+    	}
+    	echo '</table></div>';
+    	
+    	echo '<div class="information">';
+    	echo '<nav>';
+
+    	if (is_null($user_id)) echo "<a href='/search_student.php?im=$id' class='but' id='add_book'></a>";
+        else                   echo "<a href='/give.php?bk=$id&us=$user_id' class='but' id='delate_book'></a>";
+    	
+    	echo '</nav>';
+    	echo '<table class="books">';
+    	echo '<tr>';
+
+    	if (is_null($user)) {
+    		echo '<th class="td_value">Свободна</th></tr>';
+    		exit('</tr></table></div></div>');
+    	}
+
+    	if (is_null($book['Date_of_issue'])) $date = '-';
+        else                                 $date = date('d.m.Y', strtotime($book['Date_of_issue']));
+
+		echo '<th class="td_head">Ученик</th>';
+		echo "<th class='td_value'>{$user['Surname']} {$user['Firstname']} {$user['Lastname']} ";
+		echo "<span class='class'>{$user['Class']} {$user['Letter']}</span>";
+		echo '</th></tr><tr>';
+		echo '<td class="td_head">Дата выдачи: </td>';
+		echo "<td class='td_value'>$date</td>";
+
+    	exit('</tr></table></div></div>');
+	?>
+
+</body>
+	
+</html>
---
+++ search_book.php
@@ -0,0 +1,238 @@
+<?php
+
+    function filter($name, $choice) { // function for init values from GET-form
+        $regex = array('number' => '/^\d+$/', 'search' => '/^(\d|[а-я ]|[\.-])+$/ui');
+        return filter_input(INPUT_GET, $name) && preg_match($regex[$choice], $_GET[$name]) ? $_GET[$name]: false;
+    }
+    
+    $q     = filter('q', 'search'); // Init query
+    $im    = filter('im', 'number'); // Init user_id
+    $del   = filter('del','number'); // Init is_delete_page?
+    $group = filter('group', 'number'); // Init is_group_page?
+    $order = filter_input(INPUT_GET, 'order') && in_array($_GET['order'], ['author', 'genre', 'inventory_no', 'Date_of_issue']) ? $_GET['order'] : 'name'; // Init which order you ewant
+    $page  = filter('page', 'number'); // Init which books you should send to AJAX
+
+    $mysql = mysqli_connect('localhost', 'root', '', 'Lib'); // Connect to mysql
+
+    if (!$mysql) exit('<h1>Ошибка Подключения</h1>'); // If error, exit
+
+    function add_book($mysql, $q, $im, $del, $order, $group, $page=0) { // function for adding books
+
+        $query = "SELECT * FROM `books` WHERE $order LIKE '$q%'";
+
+        if ($im && $del) { // Show only busy books
+            $query = "$query AND `GROUP_ID` = 0 AND `User_id` = '$im' UNION
+                      $query AND `User_id` = '$im' GROUP BY `Group_ID` HAVING `Group_ID` > 0";
+        } elseif ($group) { // Show only books for one group
+            $query = "$query AND `GROUP_ID` = $group";
+        } else { // All books, order by value
+            $query = "$query AND `GROUP_ID` = 0 UNION
+                      $query GROUP BY `Group_ID` HAVING `Group_ID` > 0";
+        }
+
+        if ($order == 'inventory_no' || $group) $query .= " ORDER BY `Inventory_NO` LIMIT 20 OFFSET $page"; // Sort by number
+        else                                    $query .= " ORDER BY (`User_id`>0), BINARY(lower($order)) LIMIT 20 OFFSET $page"; // Sort by russian words
+
+        $result = mysqli_query($mysql, $query); // Send query
+        $bk     = mysqli_fetch_assoc($result); // Output query
+
+         if (is_null($bk)) { // If books doesn't have...
+            exit("Ничего не найдено");
+        } 
+
+        $src = '<a class="result valid "'; // Sources
+        if ($im && $del) $src .= "href='give.php?us=$im&bk=";
+        elseif ($im)     $src .= "href='get.php?us=$im&bk=";
+        else             $src .= "href='books.php/";
+
+        do { // While to adding books
+            $gid = $group || $del || ($order == 'inventory_no' && $q) ? '': $bk['Group_ID'];
+
+            if ($gid) { // If this book in group
+
+                $res1 = mysqli_query($mysql, "SELECT COUNT(*) as g FROM `books` WHERE Group_ID = '$gid'");
+                $res2 = mysqli_query($mysql, "SELECT COUNT(*) as g FROM `books` WHERE Group_ID = '$gid' AND `User_id`");
+                $all  = mysqli_fetch_assoc($res1)['g']; // Get count of books
+                $busy = mysqli_fetch_assoc($res2)['g']; // Get count of busy books
+
+                echo "<a class='result valid group' href='search_book.php?q=$q&order=$order&im=$im&del=$del&group=$gid'>";
+                echo '<div class="left_part">';
+                echo '<div class="сhoice">';
+                echo "<input type='checkbox' id='-{$bk['Group_ID']}' name='-{$bk['Group_ID']}'>";
+                echo "<label for='-{$bk['Group_ID']}'></label>";
+         		echo '</div>';
+                echo "<div class='information'>$busy/$all</div>";
+                echo '<div class="FCS">';
+                echo "<span class='name_book'>{$bk['Name']}</span>";
+                echo "<span class='autor_book'>{$bk['Author']}</span>";
+                echo '</div></div></a>';
+
+            } else { // General book without group_id 
+                $id = $bk['User_id']; // Get user id in book
+                $res = mysqli_query($mysql, "SELECT * FROM `users` WHERE `ID` = '$id'");
+                $user = mysqli_fetch_assoc($res); // Get user
+                $date = $bk['Date_of_issue'] ? date('d.m.y', strtotime($bk['Date_of_issue'])) : Null;
+
+                if ($im && !$del && !is_null($id)) echo "<a class='result'>"; // If book isn't busy
+                else                               echo $src, $bk['ID'],"'>"; // If book is busy
+
+                echo '<div class="left_part">';
+                
+                echo '<div class="сhoice">';
+                echo "<input type='checkbox' id='{$bk['ID']}' name='{$bk['ID']}'>";
+                echo "<label for='{$bk['ID']}'></label>";
+         		echo '</div>';
+                echo '<div class="FCS">';
+                echo "<span class='name_book'>{$bk['Name']}</span>";
+                echo "<span class='autor_book'>{$bk['Author']}</span>";
+                echo "<div class='date'>$date</div>";
+                echo '</div></div>';
+                echo '<div class="right_part">';
+                echo "<div class='information'>{$bk['Inventory_NO']}</div>";
+                echo '<div class="FCS">';
+
+                if (is_null($user)) echo 'Свободна'; // If book is freedom
+                else                echo $user['Surname'], ' ', $user['Firstname'], ' ', $user['Lastname']; // If not, show saibi
+
+                echo '</div></div></a>';
+            }
+
+        } while ($bk = mysqli_fetch_assoc($result));
+        
+        echo '</div>';
+    }
+
+    if ($page) { // If this is a AJAX query...
+        add_book($mysql, $q, $im, $del, $order, $group, $page);
+        exit();
+    }
+?>
+<!-- version 1.1 release -->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <title>Поиск книг</title>
+
+    <link rel="stylesheet" type="text/css" href="sources/style/button.css">
+    <link rel="stylesheet" type="text/css" href="sources/style/search.css">
+    <link rel="stylesheet" type="text/css" href="sources/style/message.css">
+    <script src="sources/js/search.js"></script>
+    <script src="sources/js/message.js"></script>
+</head>
+
+<body>
+	<div class="toolbar">
+        <div class="but" id="edit"></div>
+        <div class="but" id="del" message="delete"></div>
+    </div>
+
+	<a class="but" id="home" href="/"></a>  <!-- Button home -->
+	<a id="up" class="but hidden"></a> <!-- Button Up -->
+
+	<div class="dark" id="delete"> <!-- window -->
+        <div class="alert_window">
+            <div class="alert_message" id="result">
+                Будет удалено 34 книги. 30 из группы и 4 одиночных<br>Продолжить?
+            </div>
+            <div class="sure">
+                <div class="but_window_space">
+                    <a class="but_window" href="" id="link">Удалить</a>
+                </div>
+                <div class="but_window_space">
+                    <div class="but_window" id="cancel">Отменить</div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <?php
+        if ($group) {
+            $result = mysqli_query($mysql, "SELECT `Name` FROM `books` WHERE `Group_ID` = $group"); // Send query
+            $name   = mysqli_fetch_assoc($result); // Output name of group
+
+            if ($name) {
+                echo "<h2>Поиск книг по группе &#171;{$name['Name']}&#187; </h2>"; // Print name of group
+            } else {
+                echo '<h2>Поиск книг</h2>';   
+            }
+        } else {
+            echo '<h2>Поиск книг</h2>';
+        }	    
+
+        echo '<div class="find_input">';
+        echo '<form action="" method="GET">';
+        echo "<input type='search' name='q' value='$q' id='input' autocomplete='off' autofocus>";
+        echo '<select name="order">';
+
+        $arr = array('name' => 'Название', 'author' => 'Автор', 'inventory_no' => 'ID', 'genre' => 'Жанр', 'Date_of_issue' => 'Дата');
+
+        foreach ($arr as $i => $j) {
+            if ($i == $order) echo "<option value='$i' selected>$j</option>";
+            else              echo "<option value='$i'>$j</option>"; 
+        }
+        echo '</select>';
+
+        // If one of this params is exist
+        if ($group) echo "<input type='hidden' name='group' value='$group'>";
+        if ($im)    echo "<input type='hidden' name='im' value='$im'>";
+        if ($del)   echo "<input type='hidden' name='del' value=1>";
+
+        echo '<button type="submit" id="submit"></button>';
+        echo '</form></div>';
+        echo '<div class="search_result">';
+
+        // Call function for print first 20 books 
+        add_book($mysql, $q, $im, $del, $order, $group);
+    ?>
+
+    <script type="text/javascript">
+
+        var page = 20;
+        var allowLoading = true; // Check, if request is free
+        var is_end_of_books = false; // Check, if books is finished
+        var site = document.documentElement; // All html document
+        var list_books = document.querySelector('.search_result'); // Div for all books
+
+        function success(data) {
+            if (data != "Ничего не найдено") {
+                list_books.innerHTML += data; // Add new 20 books
+                page += 20;
+            } else {
+                is_end_of_books = true;
+            }
+        }
+
+        function ajax(url) { // Send and Get Ajax-request
+            if (!allowLoading) return
+            allowLoading = false;
+
+            var request = new XMLHttpRequest();
+
+            request.onreadystatechange = function() { // If request is comeback
+                if (request.readyState == 4 && request.status == 200) {
+                    var req = request.responseText;
+                    success(req);
+                    allowLoading = true;
+                    console.log("New stack...")
+                }
+            }
+
+            request.open('GET', url);
+            request.setRequestHeader('Content-Type', 'application/x-www-form-url');
+            request.send(); // Send ajax request
+        }
+        
+        function ready() {
+            if (!is_end_of_books && Math.floor(site.scrollTop + site.clientHeight) + 200 >= site.scrollHeight) {
+                var url = `search_book.php?q=<?php echo $q ?>&im=<?php echo $im ?>&del=<?php echo $del ?>&order=<?php echo $order ?>&group=<?php echo $group ?>&page=${page}`;
+
+                ajax(url); // Call Ajax funciton
+            }
+        }
+        document.addEventListener('scroll', ready); // Event for listen your scroll in site
+    </script>
+</body>
+</html>
