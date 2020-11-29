771ca5b17331be1d300579c0a2933035fe28a7d7
Koe-kto <65008219+Pavlik-g@users.noreply.github.com>
2020-11-29 22:40:58 +0300

    😠

--- mysite/sources/js/search.js
+++ mysite/sources/js/search.js
@@ -30,48 +30,44 @@ function scroll_control() {
     but_up.addEventListener('click', go_top)
 }
 
-function check_contol() {
-    function toolbar_contol(choise_box) {
-        var choise_box = choise_box.currentTarget
-        let show_tool = false
-        for (let i = 0; i < inputs.length; i++) {
-            if (inputs[i].checked) {
-                show_tool = true
-                break
-            }
-        }
-        toolbar_show(show_tool)
-    }
-
-    function toolbar_show(show) {
-        if (show) {
-            toolbar.classList.add("show")
-        } else {
-            toolbar.classList.remove("show")
-        }
-
-    }
-
-    let inputs = document.querySelectorAll(".сhoice input[type='checkbox']")
-    let toolbar = document.querySelector(".toolbar")
-
-
+function toolbar_control() {
+    let show_tool = false
+    let inputs = document.querySelectorAll(".choice input[type='checkbox']")
     for (let i = 0; i < inputs.length; i++) {
-        inputs[i].addEventListener("change", toolbar_contol)
+        if (inputs[i].checked) {
+            show_tool = true
+            break
+        }
+    }
+    toolbar_show(show_tool)
+}
 
+function toolbar_show(show) {
+    let toolbar = document.querySelector(".toolbar")
+    if (show) {
+        toolbar.classList.add("show")
+    } else {
+        toolbar.classList.remove("show")
     }
 }
 
+function check_control() {
+
+
+    append_listener_for_new_change()
+}
+
 function append_listener_for_new_change(how_mach) {
-    let inputs = document.querySelectorAll(".сhoice input[type='checkbox']")
-    for (let index = inputs.length - how_mach; index < inputs.length; index++) {
-        inputs[index].addEventListener("change", toolbar_contol)
+    let inputs = document.querySelectorAll(".choice input[type='checkbox']")
+    if (!how_mach) { how_mach = inputs.length }
+    for (let i = inputs.length - how_mach; i < inputs.length; i++) {
+        inputs[i].addEventListener("change", toolbar_control)
     }
 }
 
 function ready() {
     scroll_control()
-    check_contol()
+    check_control()
 }
 
 
